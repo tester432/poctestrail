5cd6605b91ebb90ce09ebb2df230258a936a2dab
Leonis Miroci <leonis.miroci@intrado.com>
2023-01-02 16:15:22 +0100

    test

---
+++ src/test/java/com/SumTests.java
@@ -0,0 +1,17 @@
+package com;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+class SumTests {
+
+    @Test
+    void AddTwoNumbers() {
+        Assert.assertEquals(3, 1 + 2, "1 + 2 should equal 3");
+    }
+
+    @Test
+    void AddTwoNumbersWithDecimals() {
+        Assert.assertEquals(3, 1.5+1.5, "1.5+1.4 should equal 3");
+    }
+}
